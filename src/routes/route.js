4c38885d501fc00716f1cf5ae8c82dc6b6b4ec8b
이동현 <dlehdanakf@konkuk.ac.kr>
2020-08-22 16:54:54 +0900

    update user route Info

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,61 +1,95 @@
 import express from "express";
-import { getAllRouteInfo, getBeaconListInRoute, getRouteInfo, updateUserLastRoute } from "@/components/database";
+import {
+  getAllRouteInfo,
+  getBeaconListInRoute,
+  getRouteInfo,
+  updateUserLastRoute,
+} from "@/components/database";
 
 const responseRouteList = (req, res) => {
-	getAllRouteInfo().catch((e)=>{
-		//res.status(520);
-		//res.set(`Content-Type`, `application/json`);
+  getAllRouteInfo()
+    .catch((e) => {
+      //res.status(520);
+      //res.set(`Content-Type`, `application/json`);
 
-		//res.send(e);
-		console.log(e);
-	}).then(rows => {
-		res.status(200);
-		res.set(`Content-Type`, `application/json`);
+      //res.send(e);
+      console.log(e);
+    })
+    .then((rows) => {
+      res.status(200);
+      res.set(`Content-Type`, `application/json`);
 
-		res.send(rows);
-	});
+      res.send(rows);
+    });
+};
+const responseUserUpdateResult = (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.body;
+  updateUserRoute(id, userId)
+    .catch((e) => {
+      //res.status(520);
+      //res.set(`Content-Type`, `application/json`);
+
+      //res.send(e);
+      console.log(e);
+    })
+    .then((rows) => {
+      res.status(200);
+      res.set(`Content-Type`, `application/json`);
+
+      res.send(`update complete!`);
+    });
 };
 const responseRouteInfo = (req, res) => {
-	const { id } = req.params;
-	getRouteInfo(id).then(([ row ]) => {
-		if(row === undefined) {
-			res.sendStatus(404);
-			return;
-		}
+  const { id } = req.params;
+  getRouteInfo(id).then(([row]) => {
+    if (row === undefined) {
+      res.sendStatus(404);
+      return;
+    }
 
-		res.status(200);
-		res.set(`Content-Type`, `application/json`);
+    res.status(200);
+    res.set(`Content-Type`, `application/json`);
 
-		getBeaconListInRoute(id).then(rows => {
-			const beacon_list = rows.map(({id, title, description, longitude, latitude}, index) => ({id, title, description, longitude, latitude, index: index + 1}));
-			res.send({...row, beacon_list});
-		});
-	});
+    getBeaconListInRoute(id).then((rows) => {
+      const beacon_list = rows.map(
+        ({ id, title, description, longitude, latitude }, index) => ({
+          id,
+          title,
+          description,
+          longitude,
+          latitude,
+          index: index + 1,
+        })
+      );
+      res.send({ ...row, beacon_list });
+    });
+  });
 };
 const startTour = (req, res) => {
-	const { id: route_id } = req.params;
-	const { user_id } = req.body;
+  const { id: route_id } = req.params;
+  const { user_id } = req.body;
 
-	updateUserLastRoute(user_id, route_id).then(_ => {
-		res.set(`Content-Type`, `application/json`);
-		res.send({ result: `complete` });
-	});
+  updateUserLastRoute(user_id, route_id).then((_) => {
+    res.set(`Content-Type`, `application/json`);
+    res.send({ result: `complete` });
+  });
 };
 const endTour = (req, res) => {
-	const { id: route_id } = req.params;
-	const { user_id } = req.body;
+  const { id: route_id } = req.params;
+  const { user_id } = req.body;
 
-	updateUserLastRoute(user_id, -1).then(_ => {
-		res.set(`Content-Type`, `application/json`);
-		res.send({ result: `complete` });
-	});
+  updateUserLastRoute(user_id, -1).then((_) => {
+    res.set(`Content-Type`, `application/json`);
+    res.send({ result: `complete` });
+  });
 };
 
-export default function() {
-	const router = express.Router();
-	router.get(`/`, responseRouteList);
-	router.get(`/:id`, responseRouteInfo);
-	router.post(`/:id`, startTour);
-	router.post(`/:id/complete`, endTour);
-	return router;
+export default function () {
+  const router = express.Router();
+  router.get(`/`, responseRouteList);
+  router.get(`/:id`, responseRouteInfo);
+  router.post(`/:id`, startTour);
+  router.post(`/:id/complete`, endTour);
+  return router;
 }
